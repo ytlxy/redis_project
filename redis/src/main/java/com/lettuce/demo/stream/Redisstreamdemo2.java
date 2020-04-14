24e7fd7ce48c6a4c2420e9ac1dbe5ea0004ef38f
ytlxy <a739771409@gmail.com>
2020-04-14 18:24:42 +0800

    new demo

---
+++ redis/src/main/java/com/lettuce/demo/stream/Redisstreamdemo2.java
@@ -0,0 +1,30 @@
+package com.lettuce.demo.stream;
+import com.lettuce.demo.util.redisutil;
+import io.lettuce.core.RedisFuture;
+import io.lettuce.core.StreamMessage;
+import io.lettuce.core.XReadArgs;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Redisstreamdemo2 {
+    private static final String STREAM_ID="message-stream";
+
+    public static void main(String[] args)throws Exception {
+        StatefulRedisConnection connection=redisutil.getConnection();
+        RedisAsyncCommands commands=connection.async();
+        XReadArgs block=XReadArgs.Builder.block(Duration.ZERO);
+        XReadArgs.StreamOffset<String> offset=XReadArgs.StreamOffset.from(STREAM_ID,"$");
+        boolean flay=true;
+        while (flay){
+            RedisFuture<List<StreamMessage<String,String>>> future=commands.xread(block,offset);
+            List<StreamMessage<String,String>>managers= future.get();
+            for(StreamMessage message : managers){
+                System.out.println("[ConsumerA]接收消息id："+message.getId()+"内存："+message.getBody());
+            }
+        }
+        redisutil.close();
+    }
+}
---
+++ redis/src/main/java/com/lettuce/demo/stream/RedisstreanSeng.java
@@ -0,0 +1,26 @@
+package com.lettuce.demo.stream;
+
+import com.lettuce.demo.util.redisutil;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class RedisstreanSeng {
+    private static final String STREAM_ID="message-stream";
+
+    public static void main(String[] args)throws Exception {
+        StatefulRedisConnection connection=redisutil.getConnection();
+        RedisCommands commands=connection.sync();
+        for(int x=0;x<10;x++){
+            TimeUnit.SECONDS.sleep(1);
+            Map<String,String> bodymap=new HashMap<String, String>();
+            bodymap.put("head","limassage-"+x);
+            bodymap.put("data","hello"+x);
+            bodymap.put("qewe","hello2"+x);
+            System.out.println("信息发送id:"+commands.xadd(STREAM_ID,bodymap));
+        }
+    }
+}
